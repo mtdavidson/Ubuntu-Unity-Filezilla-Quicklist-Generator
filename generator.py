56c7886fc87715729457bebe2f48dbc246ac2a46
Mark Davidson <mark@4each.co.uk>
2011-05-03 00:03:43 +0100

    Argument checks

--- generator.py
+++ generator.py
@@ -10,19 +10,25 @@ def getServers(xml):
     print servers;
 
     for server in servers:
-        host = server.xpathEval('Host')[0].content;
-        port = server.xpathEval('Port')[0].content;
-        user = server.xpathEval('User')[0].content;
-        password = server.xpathEval('Pass')[0].content;
+        host = server.xpathEval('Host')[0].content
+        port = server.xpathEval('Port')[0].content
+        user = server.xpathEval('User')[0].content
+        password = server.xpathEval('Pass')[0].content
 
-        #print password;
-        
     doc.freeDoc()
     ctxt.xpathFreeContext()
  
 sys.argv = ['generator.py', '/home/mtdavidson/.filezilla/sitemanager.xml'];
 
-sitemanagerFile = sys.argv[1];
+if len(sys.argv) != 2:
+    sys.exit('Incorrect Number of Arguments');
 
+if not os.path.exists(sys.argv[1]):
+    #TODO: Check is file
+    #TODO: Check is XML file
+    sys.exit('Invalid file specified');
+
+sitemanagerFile = sys.argv[2];
+    
 if __name__ == "__main__":
     getServers(sitemanagerFile)
